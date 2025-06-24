af04f229131a3cb7878c5c0cfbd3c58a7729bc59
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-06-24 13:24:55 +0700

    default animation on

--- web-reader/store/AppStore.store.ts
+++ web-reader/store/AppStore.store.ts
@@ -17,7 +17,7 @@ interface AnimationProps {
 export class AppStore {
   useLayout: boolean = true;
   profile: any = null;
-  animationMode: boolean = false;
+  animationMode: boolean = true;
   useFooter: boolean = true;
   useGameMenu: boolean = true;
   animations: AnimationProps = {};
