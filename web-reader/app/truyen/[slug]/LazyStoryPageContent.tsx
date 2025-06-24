86f5d1208c43b5035fab49b377baa7224a1eab5d
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-06-24 23:38:03 +0700

    Create LazyStoryPageContent.tsx

---
+++ web-reader/app/truyen/[slug]/LazyStoryPageContent.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const StoryDetailClient = dynamic(
+  () => import("@/components/story/StoryDetailClient"),
+  { ssr: false },
+);
+
+const LazyStoryPageContent = (props: any) => {
+  return <StoryDetailClient {...props} />;
+};
+
+export default LazyStoryPageContent;
