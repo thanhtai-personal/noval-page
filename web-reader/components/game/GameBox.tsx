500fce9254a54d2759d326dd6aee70ba34bf41a8
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-07-03 00:57:57 +0700

    Update GameBox.tsx

--- web-reader/components/game/GameBox.tsx
+++ web-reader/components/game/GameBox.tsx
@@ -14,8 +14,8 @@ export const GameBox = ({ children, onClose }: any) => {
       </div>}
       {openLotteryModal && <div
         onClick={() => setOpenLotteryModal(false)}
-        className="fixed z-[10] cursor-pointer bg-transparent top-40 right-24 uppercase gb-close-icon">
-        <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/delete-sign.png" alt="delete-sign" />
+        className="fixed z-[10] cursor-pointer bg-transparent top-16 md:top-40 right-8 md:right-24 uppercase gb-close-icon">
+        <img className="w-[40px] h-[40px] md:w-[94px] md:h-[94px]" width="94" height="94" src="https://img.icons8.com/3d-fluency/94/delete-sign.png" alt="delete-sign" />
       </div>}
       <GameMenu openLotteryModal={() => setOpenLotteryModal(true)} />
     </>
