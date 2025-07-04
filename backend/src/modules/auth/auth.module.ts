ff9bedb7894705abdcec09946f6f77f037c18527
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-07-04 09:49:42 +0700

    fix(auth): rename jwt strategy file

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '@/schemas/user.schema';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from "./jwt.stragety";
+import { JwtStrategy } from "./jwt.strategy";
 import { Role, RoleSchema } from "@/schemas/role.schema";
 import { GoogleOAuthGuard } from './guards/google-oauth.guard';
 import { GoogleStrategy } from './strategies/google.strategy';
