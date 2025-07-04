da986a689c847a545983d252ca41ae38456def9e
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-07-04 19:10:24 +0700

    new mobile app

---
+++ TangThuLauNative/app/(tabs)/profile.tsx
@@ -0,0 +1,46 @@
+import { Image } from 'expo-image';
+import { StyleSheet } from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedView } from '@/components/ThemedView';
+import GoogleLogin from "@/components/GoogleLogin";
+
+export default function ProfileScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.center}>
+        <GoogleLogin />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ TangThuLauNative/components/GoogleLogin.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Alert, View, Text } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+// Cấu hình Google Sign-In
+GoogleSignin.configure({
+  webClientId: process.env.GOOGLE_CLIENT_ID_FOR_WEB,
+  androidClientId: process.env.GOOGLE_CLIENT_ID_FOR_ANDROID,  // Client ID tạo từ Firebase
+  iosClientId: process.env.GOOGLE_CLIENT_ID_FOR_ANDROID,  // Client ID tạo từ Firebase
+  offlineAccess: false,
+} as any);
+
+export default function GoogleLogin() {
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo: any = await GoogleSignin.signIn();
+      console.log('✅ Đăng nhập thành công:', userInfo);
+      Alert.alert('Xin chào!', userInfo.user.name);
+    } catch (error: any) {
+      console.error('❌ Lỗi đăng nhập:', error);
+      Alert.alert('Lỗi', error.message);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 18, marginBottom: 16 }}>Đăng nhập bằng Google</Text>
+      <Button title="Đăng nhập với Google" onPress={signIn} />
+    </View>
+  );
+}
